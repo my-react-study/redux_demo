affde6753590ca5aadd7e9067a9c87b2f72f0c26
tangmingheng <mhtang@thoughtworks.com>
2020-02-11 21:29:38 +0800

    项目结构成功

---
+++ src/TodoList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+import store from './store'
+
+
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props)
+        //关键代码-----------start
+        this.state = store.getState();
+        //关键代码-----------end
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <div style={{ margin: '10px' }}>
+                <div>
+
+                    <Input placeholder={this.state.inputValue} style={{ width: '250px', marginRight: '10px' }} />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{ margin: '10px', width: '300px' }}>
+                    <List
+                        bordered
+                        //关键代码-----------start
+                        dataSource={this.state.list}
+                        //关键代码-----------end
+                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+export default TodoList;
