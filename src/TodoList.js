9b487f9c935daf69db1a4c6abb477fb25548a29c
tangmingheng <mhtang@thoughtworks.com>
2020-02-12 16:25:14 +0800

    组件业务UI与业务逻辑分开

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,51 +1,29 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
 import store from './store'
-import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './store/actionTypes'
 import { changeInputAction, addItemAction, deleteItemAction } from "./store/actionCreators";
+import TodoListUI from './TodoListUI';
 
 
 
 class TodoList extends Component {
     constructor(props) {
         super(props)
-        //关键代码-----------start
         this.state = store.getState();
-        //关键代码-----------end
         this.changeInputValue = this.changeInputValue.bind(this)
-        //----------关键代码-----------start
         this.storeChange = this.storeChange.bind(this)  //转变this指向
-        store.subscribe(this.storeChange) //订阅Redux的状态
-        //----------关键代码-----------end
         this.addItem = this.addItem.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
+        store.subscribe(this.storeChange) //订阅Redux的状态
     }
     render() {
         return (
-            <div style={{ margin: '10px' }}>
-                <div>
-
-                    <Input
-                        placeholder={this.state.inputValue}
-                        style={{ width: '250px', marginRight: '10px' }}
-                        onChange={this.changeInputValue}
-                        value={this.state.inputValue}
-                    />
-                    <Button
-                        type="primary"
-                        onClick={this.addItem}
-                    >增加</Button>
-                </div>
-                <div style={{ margin: '10px', width: '300px' }}>
-                    <List
-                        bordered
-                        //关键代码-----------start
-                        dataSource={this.state.list}
-                        //关键代码-----------end
-                        renderItem={(item, index) => (<List.Item onClick={this.deleteItem.bind(this, index)}>{item}</List.Item>)}
-                    />
-                </div>
-            </div>
+            <TodoListUI
+                inputValue={this.state.inputValue}
+                changeInputValue={this.changeInputValue}
+                addItem={this.addItem}
+                list={this.state.list}
+                deleteItem={this.deleteItem}
+            />
         );
     }
 
